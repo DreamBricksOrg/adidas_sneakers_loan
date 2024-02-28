b6d53f741738d3eeec052b25d4aaba2eb7a1a68b
Julio Balbino <julio4110@hotmail.com>
2024-02-28 13:38:58 -0300

    fix. rental list order by start_datetime

--- promoter.py
+++ promoter.py
@@ -104,11 +104,15 @@ def rental_list_page():
         return redirect(url_for('promoter.promoter_menu_page'))
 
     cur = mysql.connection.cursor()
-    cur.execute("SELECT Locacao.id,Tenis.tamanho AS Tenis, Usuario.nome_iniciais AS Usuario, Promotor.nome AS "
-                "Promotor, Locacao.data_inicio AS Inicio, Locacao.data_fim AS Fim, Locacao.status AS Status, "
-                "Local.nome AS Local, Locacao.Estande FROM Locacao JOIN Tenis ON Locacao.Tenis = Tenis.id JOIN Usuario ON "
-                "Locacao.Usuario = Usuario.id JOIN Promotor ON Locacao.Promotor = Promotor.id JOIN Local ON "
-                "Locacao.Local = Local.id;")
+    cur.execute("SELECT Locacao.id, Tenis.tamanho AS Tenis, Usuario.nome_iniciais AS Usuario, "
+                "Promotor.nome AS Promotor, Locacao.data_inicio AS Inicio, Locacao.data_fim AS Fim, "
+                "Locacao.status AS Status, Local.nome AS Local, Locacao.Estande "
+                "FROM Locacao "
+                "JOIN Tenis ON Locacao.Tenis = Tenis.id "
+                "JOIN Usuario ON Locacao.Usuario = Usuario.id "
+                "JOIN Promotor ON Locacao.Promotor = Promotor.id "
+                "JOIN Local ON Locacao.Local = Local.id "
+                "ORDER BY Locacao.data_inicio DESC;")
     rentals = cur.fetchall()
     print(rentals)
     cur.close()
